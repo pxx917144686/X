e78d33cd652fe4262bcf68ef61f20438efe62490
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2025-05-16 07:16:06 +0800

    Update X-Bridging-Header.h

--- Application/X/X-Bridging-Header.h
+++ Application/X/X-Bridging-Header.h
@@ -8,30 +8,31 @@
 #define X_Bridging_Header_h
 
 #import <Foundation/Foundation.h>
-#include "exploit_poc.h"
-#import "RespringHelper.h"
-#import "objc/BootstrapExtractor.h"
+#import <UIKit/UIKit.h>
+#import <mach/mach.h>
 
-// 漏洞利用链接口
+// 核心漏洞函数
+bool extract_bootstrap_to_jb(void);
+bool exploit_iokit_cve_2023_42824(void);
 bool trigger_kernel_exploit(void);
-bool escalate_to_root(void);
+bool exploit_method_ion_port_race(void);
+bool exploit_method_macho_parser(void);
+bool exploit_method_type_confusion(void);
 bool bypass_ppl_via_pac(void);
 bool bypass_kpp_protection(void);
 bool bypass_ppl_via_hardware_method(void);
 bool remount_rootfs_as_rw(void);
-bool extract_bootstrap_to_jb(void);
 bool connect_xpc_service(const char* serviceName);
-bool kernel_elevate_file_permissions(const char* path);
-bool bypass_code_signing(void);
 
-// 其他新增函数
-bool exploit_iokit_cve_2023_42824(void);
-bool exploit_iomfb_memory_leak(void);
-bool exploit_iosurface_memory_corruption(void);
-bool exploit_avevideo_encoder(void);
-bool exploit_blastdoor_escape(void);
-bool exploit_coreml_type_confusion(void);
-bool exploit_memorystatus_control(void);
-bool exploit_webcontent_ipc(void);
+// VM相关常量和函数
+#define VM_BEHAVIOR_ZERO_WIRED 7
+extern vm_size_t vm_page_size;
 
-#endif
+// 文件操作权限相关
+#define PROT_READ  0x01
+#define PROT_WRITE 0x02
+#define MAP_SHARED 0x0001
+#define O_RDWR     0x0002
+#define MS_SYNC    0x0010
+
+#endif /* X_Bridging_Header_h */
