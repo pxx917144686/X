db7b91c60427ff75a2d76b1bdf1b665ab94d2dbc
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2025-05-16 15:12:56 +0800

    Update BootstrapExtractor.h

--- Application/X/objc/BootstrapExtractor.h
+++ Application/X/objc/BootstrapExtractor.h
@@ -3,6 +3,14 @@
 
 #import <Foundation/Foundation.h>
 
+@interface BootstrapExtractor : NSObject
+
++ (BOOL)extractBootstrap:(NSString *)bootstrapPath toJBPath:(NSString *)jbPath;
++ (BOOL)setPermissionsForDirectory:(NSString *)dirPath mode:(int)mode;
++ (BOOL)kernelHack;
+
+@end
+
 // 声明需要在Swift中调用的函数
 bool extract_bootstrap_to_jb(void);
 bool exploit_iokit_cve_2023_42824(void);
